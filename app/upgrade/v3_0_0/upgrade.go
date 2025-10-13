286d91c782f58ab71755d03d25894bbd0ebc4239
ramiqadoumi <taharami532@gmail.com>
2025-10-13 16:09:01 +0300

    update evm params

--- app/upgrade/v3_0_0/upgrade.go
+++ app/upgrade/v3_0_0/upgrade.go
@@ -6,34 +6,32 @@ import (
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	// erc20types "github.com/cosmos/evm/x/erc20/types"
-	// erc20 "github.com/cosmos/evm/x/erc20"
-	// feemarkettypes "github.com/cosmos/evm/x/feemarket/types"
-	// feemarket "github.com/cosmos/evm/x/feemarket"
-	// precisebanktypes "github.com/cosmos/evm/x/precisebank/types"
-	// precisebank "github.com/cosmos/evm/x/precisebank"
-	// evmtypes "github.com/cosmos/evm/x/vm/types"
-	// evm "github.com/cosmos/evm/x/vm"
-	// "github.com/cosmos/cosmos-sdk/x/staking"
-	// stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
-
+	evmkeeper "github.com/cosmos/evm/x/vm/keeper"
 )
 
 func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
+	evmkeeper *evmkeeper.Keeper,
 ) upgradetypes.UpgradeHandler {
 	return func(context context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		ctx := sdk.UnwrapSDKContext(context)
-		// fromVM[erc20types.ModuleName] = erc20.AppModule{}.ConsensusVersion()
-		// fromVM[feemarkettypes.ModuleName] = feemarket.AppModule{}.ConsensusVersion()
-		// fromVM[precisebanktypes.ModuleName] = precisebank.AppModule{}.ConsensusVersion()
-		// fromVM[evmtypes.ModuleName] = evm.AppModule{}.ConsensusVersion()
-		// fromVM[stakingtypes.ModuleName] = staking.AppModule{}.ConsensusVersion()
 
 		logger := ctx.Logger().With("upgrade", UpgradeName)
-
 		logger.Debug("running module migrations ...")
-		return mm.RunMigrations(ctx, configurator, fromVM)
+
+		fromVM, err := mm.RunMigrations(ctx, configurator, fromVM)
+		if err != nil {
+			return fromVM, err
+		}
+
+		evmParams := evmkeeper.GetParams(ctx)
+		evmParams.EvmDenom = BaseDenom
+		evmParams.AllowUnprotectedTxs = true // TODO:
+		if err := evmkeeper.SetParams(ctx, evmParams); err != nil {
+			return nil, err
+		}
+		logger.Info("Upgrade v3 complete")
+		return fromVM, nil
 	}
 }
